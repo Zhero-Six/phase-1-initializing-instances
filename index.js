8f24d8789d51ceba502cfe50baf843bb742062ac
Zhero-Six <vmochoge@gmail.com>
2025-04-02 19:56:26 +0300

    updates index.js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your code here
+// index.js
+
+// 1. Breakfast class with food and drink
+class Breakfast {
+    constructor(food, drink) {
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+// 2. Lunch class with salad, soup, and drink
+class Lunch {
+    constructor(salad, soup, drink) {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+// 3. Dinner class with salad, soup, entree, and private dessert
+class Dinner {
+    #dessert; // Declare private field first
+    
+    constructor(salad, soup, entree, dessert) {
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this.#dessert = dessert; // Assign to private field
+    }
+}
+
+// Export classes for testing
+if (typeof module !== 'undefined') {
+    module.exports = { Breakfast, Lunch, Dinner };
+}
